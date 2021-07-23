63be78a0c23a3841a15db0267baf49015eadc519
Beata Blizińska <artur.tondera@gmail.com>
2021-07-23 07:40:01 +0200

    Init project

---
+++ lista_zakupow.py
@@ -0,0 +1,12 @@
+print('lista zakupow')
+lista_zakupow = {
+    'piekarnia': ['chleb', 'bułki', 'pączek'],
+    'warzywniak': ['marchew', 'seler', 'rukola'],
+    'metalowy': ['śrubki', 'młotek']
+}
+count = 0
+for sklep, towar in lista_zakupow.items():
+    print('Idę do sklepu:', sklep.title(), 'i kupuję:', str.title((' '.join(towar))))
+    if isinstance(towar, list):
+        count += len(towar)
+print('W sumie kupuję', count, 'produktów')
